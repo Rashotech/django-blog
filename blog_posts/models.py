17614cf8128e511a1b7b09a9f792d58d2331e130
Rasheed Ayoade <rashotech@gmail.com>
2024-07-22 00:33:23 +0100

    add comment model

--- blog_posts/models.py
+++ blog_posts/models.py
@@ -20,3 +20,12 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
