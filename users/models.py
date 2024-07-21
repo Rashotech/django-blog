7106fb8e7641e7d8c06cdc3d7b8b4c8b02b21af1
Rasheed Ayoade <rashotech@gmail.com>
2024-07-22 00:16:22 +0100

    add profile and profile update view

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django import forms
 
-# Create your models here.
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+    def __init__(self, *args, **kwargs):
+        super(UserUpdateForm, self).__init__(*args, **kwargs)
+        # Make the username and email fields read-only
+        self.fields['username'].disabled = True
+        self.fields['email'].disabled = True
--- users/views.py
+++ users/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect 
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm 
 from django.contrib.auth import login, logout
+from django.core.paginator import Paginator
+from django.contrib import messages
+from blog_posts.models import BlogPost
+from .models import UserUpdateForm
+from django.contrib.auth.decorators import login_required
 
 def register_view(request):
     if request.method == "POST": 
@@ -27,3 +32,26 @@ def logout_view(request):
     if request.method == "POST": 
         logout(request) 
         return redirect("/")
+    
+@login_required(login_url="/users/login/")
+def profile_view(request): 
+    posts_list = BlogPost.objects.filter(author=request.user).order_by('-created_at')
+    paginator = Paginator(posts_list, 5)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    form = UserUpdateForm(instance=request.user)
+
+    return render(request, 'users/profile.html', {'page_obj': page_obj, 'form': form})
+
+@login_required(login_url="/users/login/")
+def profile_update(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated successfully!')
+            return redirect('users:profile')
+        else:
+            messages.error(request, 'Failed to update profile.')
