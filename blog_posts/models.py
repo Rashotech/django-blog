c19de26d87df153f61c81e00e21ce34fe610d2f0
Rasheed Ayoade <rashotech@gmail.com>
2024-07-21 11:32:30 +0100

    add blog post category and blog posts model and migration

--- blog_posts/models.py
+++ blog_posts/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- blogsite/settings.py
+++ blogsite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'theme',
     'widget_tweaks',
     'users',
+    'blog_posts',
 ]
 
 MIDDLEWARE = [
